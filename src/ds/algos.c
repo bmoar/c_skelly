b1c003a8c590d04f3a36133974b8ef17894f6cdf
bmoar <pyjedi@gmail.com>
2014-03-31 11:33:36 -0400

    Move hashing algos to algos.{c,h} for reuse

---
+++ src/ds/algos.c
@@ -0,0 +1,68 @@
+#include <ds/algos.h>
+
+uint32_t jenkins_hash(void *a) {
+    size_t len = blength((bstring)a);
+    char *key = bdata((bstring)a);
+    uint32_t hash = 0;
+    uint32_t i = 0;
+
+    for(hash = i = 0; i < len; i++) {
+        hash += key[i];
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
+    }
+
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+
+    return hash;
+}
+
+uint32_t SuperFashHash(void *a) {
+    uint32_t len = blength((bstring)a);
+    char *data = bdata((bstring)a);
+    uint32_t hash = 0;
+    uint32_t tmp = 0;
+    int rem;
+
+    if (len <= 0 || data == NULL) return 0;
+
+    rem = len & 3;
+    len >>= 2;
+
+    /* Main loop */
+    for (;len > 0; len--) {
+        hash  += get16bits (data);
+        tmp    = (get16bits (data+2) << 11) ^ hash;
+        hash   = (hash << 16) ^ tmp;
+        data  += 2*sizeof (uint16_t);
+        hash  += hash >> 11;
+    }
+
+    /* Handle end cases */
+    switch (rem) {
+        case 3: hash += get16bits (data);
+                hash ^= hash << 16;
+                hash ^= ((signed char)data[sizeof (uint16_t)]) << 18;
+                hash += hash >> 11;
+                break;
+        case 2: hash += get16bits (data);
+                hash ^= hash << 11;
+                hash += hash >> 17;
+                break;
+        case 1: hash += (signed char)*data;
+                hash ^= hash << 10;
+                hash += hash >> 1;
+    }
+
+    /* Force "avalanching" of final 127 bits */
+    hash ^= hash << 3;
+    hash += hash >> 5;
+    hash ^= hash << 4;
+    hash += hash >> 17;
+    hash ^= hash << 25;
+    hash += hash >> 6;
+
+    return hash;
+}
--- src/ds/hashmap.h
+++ src/ds/hashmap.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <ds/darray.h>
+#include <ds/algos.h>
 
 #define DEFAULT_NUMBER_OF_BUCKETS 100
 
@@ -61,31 +62,5 @@ int Hashmap_traverse(Hashmap *map, Hashmap_traverse_cb traverse_cb);
  * NULL on error
  */
 void *Hashmap_delete(Hashmap *map, void *key);
-
-/* 
- * Hash functions
- */
-
-/*
- * Paul Hsieh's SuperFashHash (SFH)
- */
-#undef get16bits
-#if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
-  || defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
-#define get16bits(d) (*((const uint16_t *) (d)))
-#endif
-
-#if !defined (get16bits)
-#define get16bits(d) ((((uint32_t)(((const uint8_t *)(d))[1])) << 8)\
-                       +(uint32_t)(((const uint8_t *)(d))[0]) )
-#endif
-
-uint32_t SuperFashHash(void *a);
-// end SFH 
-
-/*
- * Bob Jenkins hash from wikipedia
- */
-uint32_t jenkins_hash(void *a);
 
 #endif
