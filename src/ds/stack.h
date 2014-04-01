62d873aeca84079191fc457e1d57d61ac2533dfb
bmoar <pyjedi@gmail.com>
2014-04-01 09:45:39 -0400

    Add stack implimentation and tests

---
+++ src/ds/stack.h
@@ -0,0 +1,24 @@
+#include <ds/list.h>
+
+typedef struct Stack {
+    int count;
+    ListNode *first;
+    ListNode *last;
+} Stack;
+
+#define STACK_FOREACH(S, V) \
+    ListNode *V = NULL;\
+    ListNode *_node = NULL;\
+    for(V = _node = S->last; _node != NULL; V = _node = _node->prev)
+
+#define Stack_create() ((Stack *)List_create())
+
+#define Stack_destroy(A) (List_destroy((Stack *)A))
+
+#define Stack_push(A, D) (List_push((List *)A, D))
+
+#define Stack_pop(A) (List_pop((List *)A))
+
+#define Stack_peek(A) (List_last(A))
+
+#define Stack_count(A) (List_count(A))
